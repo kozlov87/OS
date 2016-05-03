0597f02eca37ff4a4326bb196850ec7e8d87f6b9
egor <ekozlov.96@gmail.com>
2016-05-03 12:18:53 +0300

    ok

--- sigusr/sigusr.c
+++ sigusr/sigusr.c
@@ -5,45 +5,54 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-
+	
 const int TIME = 10;
-volatile sig_atomic_t e_flag = 0;
+
+volatile sig_atomic_t sig_status = 0;
 
 void sig_handler(int signo, siginfo_t *siginfo, void *context) {
+	sig_status = 1;
     pid_t sender_pid = siginfo->si_pid;
-    if (signo == SIGUSR1 || signo == SIGUSR2) {
-        if(e_flag == 0) {
-            e_flag = 1;
-            printf("%s from %d\n", (signo == SIGUSR1 ? "SIGUSR1" : (signo == SIGUSR2 ? "SIGUSR2" : "")), (int)sender_pid);
-        }
-        exit(signo);
-    }
+
+	printf("signal %d from: %d\n", signo, (int)sender_pid);
+	exit(signo);
 }
 
 int main() {
     struct sigaction sigact;
     sigact.sa_sigaction = *sig_handler;
     sigact.sa_flags |= SA_SIGINFO;
-
+    
     sigset_t block_mask;
-    sigemptyset(&block_mask);
-    sigaddset (&block_mask, SIGUSR1);
-    sigaddset (&block_mask, SIGUSR2);
+	sigfillset(&block_mask);
 
+	int count = 2;
+	int cant_handle[] = {SIGKILL, 19};
+   
     sigact.sa_mask = block_mask;
-
-    if (sigaction(SIGUSR1, &sigact, NULL) != 0) {
-        printf("error\n");
-        return errno;
-    }
-
-    if (sigaction(SIGUSR2, &sigact, NULL) != 0) {
-        printf("error\n");
-        return errno;
-    }
-
+	int i;
+	for(i = 1; i < 32; ++i) {
+		int j;
+		char isOk = 1;
+		for(j = 0; j < count; ++j) {
+			if(i == cant_handle[j]) {
+				isOk = 0;
+				break;
+			}
+		} 
+		if (isOk && sigaction(i, &sigact, NULL) != 0) {
+        		printf("%d error\n", i);
+       		 	return errno;
+    		}
+	}
+	 
     sleep(TIME);
-    printf("No signals were caught\n");
 
+	sigprocmask(SIG_BLOCK, &sigact.sa_mask, 0);
+
+	if(!sig_status) {
+    	printf("No signals were caught\n");
+	}
     return 0;
 }
+
